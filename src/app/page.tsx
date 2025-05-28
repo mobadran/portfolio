8385102f0bec9d3fea6ce4d06bd87978be071242
mobadran <mohamedbadran1492008@gmail.com>
2025-05-28 17:08:43 +0300

    Added Loading terminal component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 import Header from '@/components/Header';
+import Loading from '@/components/Loading';
 import TerminalGrid from '@/components/TerminalGrid';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
   const childRef = useRef<{
     childMethod: () => void;
     spawnTerminal?: () => void;
@@ -23,16 +25,20 @@ export default function Home() {
   return (
     <>
       <Header />
-      <TerminalGrid ref={childRef} />
+      <main>
+        {isLoading ? (
+          <Loading
+            unmount={() => {
+              setIsLoading(false);
+            }}
+          />
+        ) : (
+          <TerminalGrid ref={childRef} />
+        )}
+      </main>
       <p className="mx-auto mb-2 text-center text-xs text-gray-500">
-        Photo by{' '}
-        <a href="https://unsplash.com/@betagamma?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-          Daniil Silantev
-        </a>{' '}
-        on{' '}
-        <a href="https://unsplash.com/photos/mountains-are-silhouetted-against-a-vibrant-sunrise-6VhIHgqo5qI?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-          Unsplash
-        </a>
+        Photo by <a href="https://unsplash.com/@betagamma?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Daniil Silantev</a> on{' '}
+        <a href="https://unsplash.com/photos/mountains-are-silhouetted-against-a-vibrant-sunrise-6VhIHgqo5qI?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
       </p>
     </>
   );
