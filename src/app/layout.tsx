fac40853fdd55e7f7612209c1493679e982a4427
mobadran <mohamedbadran1492008@gmail.com>
2025-06-01 14:20:53 +0300

    Added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import './globals.css';
 import { ScreenProvider } from '@/context/ScreenContext';
 
+export const metadata = {
+  title: "Badraan's Land",
+  description:
+    "Muhammad Badraan's interactive terminal-style portfolio showcasing MERN stack development skills, projects, and experience. Features a unique terminal interface with multiple windows and keyboard navigation.",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -8,17 +14,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta
-          name="description"
-          content="Muhammad Badraan's interactive terminal-style portfolio showcasing MERN stack development skills, projects, and experience. Features a unique terminal interface with multiple windows and keyboard navigation."
-        />
-
-        <title>Badraan&apos; s Terminal</title>
-        <link rel="icon" href="/favicon.ico" />
-      </head>
       <body>
         <ScreenProvider>{children}</ScreenProvider>
       </body>
